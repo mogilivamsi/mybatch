e09045725ab8b1f0d60e70e93f047f7c9f001902
Harsha gupta <harsh.gupta@protonshub.com>
2022-08-09 11:00:37 +0530

    i created user

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_09_052536) do
+ActiveRecord::Schema.define(version: 2022_08_09_053001) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.string "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
